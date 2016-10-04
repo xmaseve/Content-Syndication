16e03d446c2cfedf1467d293fb52c257aff636c0
Yi Qi <qiyi1224@gmail.com>
2016-10-04 11:04:17 -0400

    Add files via upload

--- content syndication.py
+++ content syndication.py
@@ -8,9 +8,9 @@ Created on Mon Oct 03 12:53:00 2016
 import pandas as pd
 
 #two ways to read excel file in python
-xl = pd.ExcelFile("C:\\Users\\Qi Yi\\Desktop\inSegment - Arbor Networks - Weekly Lead Report 7.7.2016.xlsx")
-leads = xl.parse("All Leads") #sheet name
-#leads = pd.read_excel('C:\\Users\\Qi Yi\\Desktop\inSegment - Arbor Networks - Weekly Lead Report 7.7.2016.xlsx', sheetname="All Leads")
+#xl = pd.ExcelFile("C:\\Users\\Qi Yi\\Desktop\inSegment - Arbor Networks - Weekly Lead Report 7.7.2016.xlsx")
+#leads = xl.parse("All Leads") #sheet name
+leads = pd.read_excel('C:\\Users\\Qi Yi\\Desktop\inSegment - Arbor Networks - Weekly Lead Report 7.7.2016.xlsx', sheetname="All Leads")
 
 unique = leads.drop_duplicates()
 len(unique) #16153
@@ -22,7 +22,29 @@ flag.to_excel("C:\\Users\\Qi Yi\\Desktop\\flag.xlsx")
 assets = ["DDoS_Security_Myths_Busted", "SecCurrent_The_Hunted_Becomes_the_Hunter",\
           "ITHarvest_Report_Security_Analytics__A_Required_Escalation_In_Cyber_Defense",\
           "Security_Beyond_the_SIEM", "OVUM_Dispelling_the_Myths_Around_DDoS"]
+
+industry = ["Banking/Financial Services", "Cloud Hosting Provider", "Computer Hardware/Software",\
+            "Entertainment", "Federal/National Government", "Healthcare/Hospitals",\
+            "Hospitality/Leisure", "Insurance", "Internet Service Provider", "Logistics/Transportation",\
+            "Managed Security Service Provider", "Media/Publishing", "Mobile Provider",\
+            "Online Gaming", "Pharmaceutical/Medical Devices", "Retail/Online Services", "Utilities"]          
+
+            
 def filterReport(unique):
-    unique[unique["Country"].isin(["US", "UK"])] &\
-    unique[unique["Most Recent Asset Download"].isin(assets)] &\
-    unique[unique["Role"].isin(["IT", "Secutiry"])]
+    a = unique[unique["Country"].isin(["US", "US of America", "UK"])] 
+    b = a[a["Most Recent Asset Download"].isin(assets)] 
+    c = b.dropna(subset=["Role"]) #Role columns has NAs.
+    d = c[c["Role"].str.contains('IT|Security')]
+    e = d[d["Industry"].isin(industry)]
+    f = e[e["Job Title"].str.contains('VP|Director|Manager|CTO|Chief|CIO|Vice|COO|CRO|CEO|\
+         CCO|CFO|CISO|C Level|V.P')]
+    g = f[f["Country"].isin(["US", "US of America"])]
+    us = g[g["Number Of Employees"].isin(["1000-4999", "5000-10000", "10001 or More"])]
+    i = f[f["Country"].isin(["UK"])]
+    uk = i[i["Number Of Employees"].isin(["500-999", "1000-4999", "5000-10000", "10001 or More"])]
+    result = pd.concat([us, uk])
+    return result
+              
+result = filterReport(unique)    
+    
+result.to_excel("C:\\Users\\Qi Yi\\Desktop\\result.xlsx") 
